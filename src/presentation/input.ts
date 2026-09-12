7e50d5b5d1cccb37ff29d1d53e50a203022b7fb6
lacourse2315-code <lacourse2315@gmail.com>
2026-09-12 00:13:47 -0400

    ux: add immediate touch feedback

--- src/presentation/input.ts
+++ src/presentation/input.ts
@@ -2,7 +2,26 @@ import type Phaser from 'phaser';
 
 export type TapAction = () => void;
 
+type Touchable = Phaser.GameObjects.GameObject & {
+  setScale?: (x: number, y?: number) => unknown;
+  setAlpha?: (alpha: number) => unknown;
+};
+
 export function bindTap(target: Phaser.GameObjects.GameObject, action: TapAction): void {
   target.setInteractive({ useHandCursor: true });
-  target.on('pointerup', action);
+  const touchable = target as Touchable;
+  const pressed = () => {
+    touchable.setScale?.(0.96);
+    touchable.setAlpha?.(0.82);
+  };
+  const released = () => {
+    touchable.setScale?.(1);
+    touchable.setAlpha?.(1);
+  };
+  target.on('pointerdown', pressed);
+  target.on('pointerout', released);
+  target.on('pointerup', () => {
+    released();
+    action();
+  });
 }
